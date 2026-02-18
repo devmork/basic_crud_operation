b835a0bcc6c0a86edb744a597db4085117d0e54b
devmork <enoymarkden@gmail.com>
2026-02-18 21:39:40 +0800

    refactor action.php to streamline event handling; improve code structure for add and update operations

--- action.php
+++ action.php
@@ -17,3 +17,30 @@ if(isset($_POST['add'])) {
     header("Location: index.php");
     exit();
 }
+
+
+if(isset($_POST['update'])) {
+    $id = $_GET['id'];
+    $event_id = $_POST['event_id'];
+    $event_date = $_POST['event_date'];
+    $start_time = $_POST['start_time'];
+    $end_time = $_POST['end_time'];
+    $status = $_POST['status'];
+    $expected_attendees = $_POST['attendees'];
+    $venue = $_POST['venue'];
+    $description = $_POST['description'];
+
+    mysqli_query($conn, "UPDATE events 
+                         SET event_id='$event_id', 
+                             event_date='$event_date', 
+                             start_time='$start_time', 
+                             end_time='$end_time', 
+                             status='$status', 
+                             expected_attendees='$expected_attendees', 
+                             venue='$venue', 
+                             description='$description' 
+                        WHERE id = '$id'");
+    
+    header("Location: index.php");
+    exit();
+}
